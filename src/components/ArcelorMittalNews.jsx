2d1a182d6a3ba86dc2521cf9728fe15451b24101
Julia <julia.bongon@gmail.com>
2024-12-11 14:21:57 +0100

    Fix integration of layout in code

--- src/components/ArcelorMittalNews.jsx
+++ src/components/ArcelorMittalNews.jsx
@@ -6,18 +6,12 @@ import { formatDate } from "../utils/dateUtils";
 import deduplicateArticles from "../utils/deduplicateNews";
 import filterRemovedArticles from "../utils/filterRemovedArticles";
 
-const ArcelorMittalNews = ({
-  language,
-  startDate,
-  endDate,
-  setFetchedNews,
-}) => {
+const ArcelorMittalNews = ({ language, startDate, endDate, setFetchedNews, fetchedNews }) => {
   const [sortBy, setSortBy] = useState("publishedAt");
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const keywords = "ArcelorMittal";
         const response = await axios.get("https://newsapi.org/v2/everything", {
           params: {
             apiKey: process.env.REACT_APP_API_KEY,
@@ -32,6 +26,7 @@ const ArcelorMittalNews = ({
         const filteredArticles = filterRemovedArticles(response.data.articles);
         const uniqueArticles = deduplicateArticles(filteredArticles);
 
+        // Set the fetched news
         setFetchedNews(uniqueArticles);
       } catch (error) {
         console.error("Error fetching ArcelorMittal news:", error);
@@ -41,81 +36,29 @@ const ArcelorMittalNews = ({
     fetchNews();
   }, [language, sortBy, startDate, endDate, setFetchedNews]);
 
-  // const title =
-  //   language === "en" ? "ArcelorMittal News" : "ArcelorMittal Nieuws";
+  const title =
+    language === "en" ? "ArcelorMittal News" : "ArcelorMittal Nieuws";
 
-  // const sortOptions =
-  //   language === "en"
-  //     ? [
-  //         { value: "relevancy", label: "Relevancy" },
-  //         { value: "popularity", label: "Popularity" },
-  //         { value: "publishedAt", label: "Newest first" },
-  //       ]
-  //     : [
-  //         { value: "relevancy", label: "Relevantie" },
-  //         { value: "popularity", label: "Populariteit" },
-  //         { value: "publishedAt", label: "Nieuwste eerst" },
-  //       ];
+  const sortOptions =
+    language === "en"
+      ? [
+          { value: "relevancy", label: "Relevancy" },
+          { value: "popularity", label: "Popularity" },
+          { value: "publishedAt", label: "Newest first" },
+        ]
+      : [
+          { value: "relevancy", label: "Relevantie" },
+          { value: "popularity", label: "Populariteit" },
+          { value: "publishedAt", label: "Nieuwste eerst" },
+        ];
 
-  // return (
-  //   <div div style={{ padding: "20px" }}>
-  //     <h1>{title}</h1>
-  //     <div>
-  //       <label htmlFor="sortBy" style={{ marginRight: "10px" }}></label>
-  //       <select
-  //         id="sortBy"
-  //         value={sortBy}
-  //         onChange={(e) => setSortBy(e.target.value)}
-  //       >
-  //         {sortOptions.map((option) => (
-  //           <option key={option.value} value={option.value}>
-  //             {option.label}
-  //           </option>
-  //         ))}
-  //       </select>
-  //     </div>
-  //     <ul style={{ listStyleType: "none", padding: 0 }}>
-  //       {newsArticles.map((article, index) => (
-  //         <li key={index} style={{ marginBottom: "20px" }}>
-  //           <h2>
-  //             <a href={article.url} target="_blank" rel="noopener noreferrer">
-  //               {article.title}
-  //             </a>
-  //           </h2>
-  //           <div style={{ display: "flex", alignItems: "flex-start" }}>
-  //             <div style={{ flex: 1 }}>
-  //               <p>{article.description}</p>
-  //               <p>
-  //                 {article.source.name},{" "}
-  //                 {formatDate(article.publishedAt, language)}
-  //               </p>
-  //             </div>
-  //             {article.urlToImage && (
-  //               <img
-  //                 src={article.urlToImage}
-  //                 alt={article.title}
-  //                 style={{
-  //                   width: "200px",
-  //                   height: "auto",
-  //                   marginLeft: "20px",
-  //                   borderRadius: "8px",
-  //                 }}
-  //               />
-  //             )}
-  //           </div>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
-  return null;
   const noNewsFound =
     language === "en"
       ? "No news found for the selected dates."
       : "Geen nieuws gevonden voor de geselecteerde periode.";
 
   return (
-    <div div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px" }}>
       <h1>{title}</h1>
       <div>
         <label htmlFor="sortBy" style={{ marginRight: "10px" }}></label>
@@ -131,11 +74,11 @@ const ArcelorMittalNews = ({
           ))}
         </select>
       </div>
-      {newsArticles.length === 0 ? (
+      {fetchedNews.length === 0 ? (
         <p style={{ color: "red", fontWeight: "bold" }}>{noNewsFound}</p>
       ) : (
         <ul style={{ listStyleType: "none", padding: 0 }}>
-          {newsArticles.map((article, index) => (
+          {fetchedNews.map((article, index) => (
             <li key={index} style={{ marginBottom: "20px" }}>
               <h2>
                 <a href={article.url} target="_blank" rel="noopener noreferrer">
@@ -146,8 +89,7 @@ const ArcelorMittalNews = ({
                 <div style={{ flex: 1 }}>
                   <p>{article.description}</p>
                   <p>
-                    {article.source.name},{" "}
-                    {formatDate(article.publishedAt, language)}
+                    {article.source.name}, {formatDate(article.publishedAt, language)}
                   </p>
                 </div>
                 {article.urlToImage && (
@@ -170,5 +112,6 @@ const ArcelorMittalNews = ({
     </div>
   );
 };
+
 
 export default ArcelorMittalNews;
--- src/components/GeneralNews.jsx
+++ src/components/GeneralNews.jsx
@@ -6,7 +6,7 @@ import { formatDate } from "../utils/dateUtils";
 import deduplicateArticles from "../utils/deduplicateNews";
 import filterRemovedArticles from "../utils/filterRemovedArticles";
 
-const GeneralNews = ({ language, startDate, endDate, setFetchedNews }) => {
+const GeneralNews = ({ language, startDate, endDate, setFetchedNews, fetchedNews }) => {
   const [sortBy, setSortBy] = useState("publishedAt");
 
   useEffect(() => {
@@ -29,8 +29,9 @@ const GeneralNews = ({ language, startDate, endDate, setFetchedNews }) => {
         });
 
         const filteredArticles = filterRemovedArticles(response.data.articles);
-
         const uniqueArticles = deduplicateArticles(filteredArticles);
+        
+        // Update the fetched news in the parent component
         setFetchedNews(uniqueArticles);
       } catch (error) {
         console.error("Error fetching news:", error);
@@ -40,84 +41,31 @@ const GeneralNews = ({ language, startDate, endDate, setFetchedNews }) => {
     fetchNews();
   }, [language, sortBy, startDate, endDate, setFetchedNews]);
 
-  // const title =
-  //   language === "en"
-  //     ? "Steel Industry General News"
-  //     : "Staal Industrie Algemeen Nieuws";
+  const title =
+    language === "en"
+      ? "Steel Industry General News"
+      : "Staal Industrie Algemeen Nieuws";
 
-  // const sortOptions =
-  //   language === "en"
-  //     ? [
-  //         { value: "relevancy", label: "Relevancy" },
-  //         { value: "popularity", label: "Popularity" },
-  //         { value: "publishedAt", label: "Newest first" },
-  //       ]
-  //     : [
-  //         { value: "relevancy", label: "Relevantie" },
-  //         { value: "popularity", label: "Populariteit" },
-  //         { value: "publishedAt", label: "Nieuwste eerst" },
-  //       ];
+  const sortOptions =
+    language === "en"
+      ? [
+          { value: "relevancy", label: "Relevancy" },
+          { value: "popularity", label: "Popularity" },
+          { value: "publishedAt", label: "Newest first" },
+        ]
+      : [
+          { value: "relevancy", label: "Relevantie" },
+          { value: "popularity", label: "Populariteit" },
+          { value: "publishedAt", label: "Nieuwste eerst" },
+        ];
 
-  // return (
-  //   <div div style={{ padding: "20px" }}>
-  //     <h1>{title}</h1>
-  //     <div style={{ marginBottom: "20px" }}>
-  //       <label htmlFor="sortBy" style={{ marginRight: "10px" }}></label>
-  //       <select
-  //         id="sortBy"
-  //         value={sortBy}
-  //         onChange={(e) => setSortBy(e.target.value)}
-  //       >
-  //         {sortOptions.map((option) => (
-  //           <option key={option.value} value={option.value}>
-  //             {option.label}
-  //           </option>
-  //         ))}
-  //       </select>
-  //     </div>
-
-  //     <ul style={{ listStyleType: "none", padding: 0 }}>
-  //       {newsArticles.map((article, index) => (
-  //         <li key={index} style={{ marginBottom: "20px" }}>
-  //           <h2>
-  //             <a href={article.url} target="_blank" rel="noopener noreferrer">
-  //               {article.title}
-  //             </a>
-  //           </h2>
-  //           <div style={{ display: "flex", alignItems: "flex-start" }}>
-  //             <div style={{ flex: 1 }}>
-  //               <p>{article.description}</p>
-  //               <p>
-  //                 {article.source.name},{" "}
-  //                 {formatDate(article.publishedAt, language)}
-  //               </p>
-  //             </div>
-  //             {article.urlToImage && (
-  //               <img
-  //                 src={article.urlToImage}
-  //                 alt={article.title}
-  //                 style={{
-  //                   width: "200px",
-  //                   height: "auto",
-  //                   marginLeft: "20px",
-  //                   borderRadius: "8px",
-  //                 }}
-  //               />
-  //             )}
-  //           </div>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
-  return null;
   const noNewsFound =
     language === "en"
       ? "No news found for the selected dates"
       : "Geen nieuws gevonden voor de geselecteerde datumbereik";
 
   return (
-    <div div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px" }}>
       <h1>{title}</h1>
       <div style={{ marginBottom: "20px" }}>
         <label htmlFor="sortBy" style={{ marginRight: "10px" }}></label>
@@ -134,11 +82,11 @@ const GeneralNews = ({ language, startDate, endDate, setFetchedNews }) => {
         </select>
       </div>
 
-      {newsArticles.length === 0 ? (
+      {fetchedNews.length === 0 ? (
         <p>{noNewsFound}</p>
       ) : (
         <ul style={{ listStyleType: "none", padding: 0 }}>
-          {newsArticles.map((article, index) => (
+          {fetchedNews.map((article, index) => (
             <li key={index} style={{ marginBottom: "20px" }}>
               <h2>
                 <a href={article.url} target="_blank" rel="noopener noreferrer">
@@ -149,8 +97,7 @@ const GeneralNews = ({ language, startDate, endDate, setFetchedNews }) => {
                 <div style={{ flex: 1 }}>
                   <p>{article.description}</p>
                   <p>
-                    {article.source.name},{" "}
-                    {formatDate(article.publishedAt, language)}
+                    {article.source.name}, {formatDate(article.publishedAt, language)}
                   </p>
                 </div>
                 {article.urlToImage && (
