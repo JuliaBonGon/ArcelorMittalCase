fac2d8314f0b3cebc922e4474ec514b9951eae1e
Eneas Heyse <eneasheyse@gmail.com>
2024-12-11 10:28:02 +0100

    some changes to shareForm from testing

--- src/components/ShareForm/ShareForm.jsx
+++ src/components/ShareForm/ShareForm.jsx
@@ -5,22 +5,21 @@ export default function ShareForm({ article }) {
     return (
         <>
             <div id="shareFormWrapper">
-                <div>
-                    <Link to="/tab">
-                        <button>Go Back</button>
-                    </Link>
-                    <h2 id="shareFormTitle">Who do you want to share this to?</h2>
-                    <div id="inputWrapper">
-                        <label htmlFor="personInput">Select a contact</label>
-                        <select id="personInput">
-                            <option>John</option>
-                            <option>Bob</option>
-                            <option>Patrick</option>
-                            <option>Sally</option>
-                        </select>
-                    </div>
+                <Link id="goBackSubmit" to="/tab">
+                    <button>Go Back</button>
+                </Link>
+                <h2 id="shareFormTitle">Who do you want to share this to?</h2>
+                <div id="inputWrapper">
+                    <label id="personInputLabel" htmlFor="personInput">Select a contact</label>
+                    <select id="personInput">
+                        <option>John</option>
+                        <option>Bob</option>
+                        <option>Patrick</option>
+                        <option>Sally</option>
+                    </select>
                 </div>
+                <button id="personInputSubmit">SHARE</button>
             </div>
         </>
-    )
+    );
 }
