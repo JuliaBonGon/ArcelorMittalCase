91c5ba2949b93a1d7d412067ef33539d98a227af
Eneas Heyse <eneasheyse@gmail.com>
2024-12-11 11:15:40 +0100

    create whitespace and positioning fixes in ShareForm, add the article title

--- src/components/ShareForm/ShareForm.jsx
+++ src/components/ShareForm/ShareForm.jsx
@@ -4,21 +4,24 @@ import "./shareForm.css";
 export default function ShareForm({ article }) {
     return (
         <>
-            <div id="shareFormWrapper">
-                <Link id="goBackSubmit" to="/tab">
-                    <button>Go Back</button>
-                </Link>
-                <h2 id="shareFormTitle">Who do you want to share this to?</h2>
-                <div id="inputWrapper">
-                    <label id="personInputLabel" htmlFor="personInput">Select a contact</label>
-                    <select id="personInput">
-                        <option>John</option>
-                        <option>Bob</option>
-                        <option>Patrick</option>
-                        <option>Sally</option>
-                    </select>
+            <div id="pageWrapper">
+                <h1 id="articleTitle">{article.title}</h1>
+                <div id="shareFormWrapper">
+                    <Link id="goBackSubmit" to="/tab">
+                        <button>Go Back</button>
+                    </Link>
+                    <h2 id="shareFormTitle">Who do you want to share this to?</h2>
+                    <div id="inputWrapper">
+                        <label id="personInputLabel" htmlFor="personInput">Select a contact</label>
+                        <select id="personInput">
+                            <option>John</option>
+                            <option>Bob</option>
+                            <option>Patrick</option>
+                            <option>Sally</option>
+                        </select>
+                    </div>
+                    <button id="personInputSubmit">SHARE</button>
                 </div>
-                <button id="personInputSubmit">SHARE</button>
             </div>
         </>
     );
